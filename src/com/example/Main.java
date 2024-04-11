95c949d8eb5ad9dded985468c571f548d300756b
Jarred <reesal.jarrederzando@seedtech.ph>
2024-04-11 15:09:51 +0800

    init commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Calling from the main func with lucy in the sky with diamonds.");
+	}
+}
